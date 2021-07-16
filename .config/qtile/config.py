5f3d6c8f2d3a2b5e9694f04efc6edd78d24ccc5c
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-07-16 12:46:34 +0300

    Added window swallowing

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -35,6 +35,7 @@ from libqtile.log_utils import logger
 import os
 import subprocess
 import sys
+import psutil
 
 mod = "mod4"
 alt = "mod1"
@@ -280,4 +281,24 @@ wmname = "LG3D"
 def autostart():
     home = os.path.expanduser("~")
     subprocess.Popen([home + "/.config/qtile/autostart.sh"])
+
+@hook.subscribe.client_new
+def _swallow(window):
+    pid = window.window.get_net_wm_pid()
+    ppid = psutil.Process(pid).ppid()
+    cpids = {c.window.get_net_wm_pid(): wid for wid, c in window.qtile.windows_map.items()}
+    for i in range(5):
+        if not ppid:
+            return
+        if ppid in cpids:
+            parent = window.qtile.windows_map.get(cpids[ppid])
+            parent.minimized = True
+            window.parent = parent
+            return
+        ppid = psutil.Process(ppid).ppid()
+
+@hook.subscribe.client_killed
+def _unswallow(window):
+    if hasattr(window, 'parent'):
+        window.parent.minimized = False
 
