39006f8f1fdea00325dbdc448932038c54925b4a
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-08-15 16:00:12 +0300

    Remove old configs

--- .config/i3/scripts/lock.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-LOCK_IMAGE=$(dirname "$0")/lock-image.png
-ORIGINAL_WALLPAPER_FILE=$(awk -F '=' '/file/ {print $2}' ~/.config/nitrogen/bg-saved.cfg)
-
-convert -blur 0x10 $ORIGINAL_WALLPAPER_FILE $LOCK_IMAGE
-
-i3lock -i $LOCK_IMAGE
-
