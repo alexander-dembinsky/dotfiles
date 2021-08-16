92a9d45676ac02bd21954516896b5b8027a42e4a
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-08-16 13:51:22 +0300

    Added icons for shutdown menu

--- .config/qtile/scripts/shutdown_menu.sh
+++ .config/qtile/scripts/shutdown_menu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 (cat <<EOF
-Logout,      qtile cmd-obj -o cmd -f shutdown
-Suspend,     systemctl suspend
-Reboot,      systemctl reboot
+Logout,      qtile cmd-obj -o cmd -f shutdown, system-log-out
+Suspend,     systemctl -i suspend            , system-suspend
+Reboot,      systemctl -i reboot             , system-reboot
 ^sep()
-Power Off,   systemctl poweroff
+Power Off,   systemctl -i poweroff           , system-shutdown
 EOF
 ) | jgmenu --simple --at-pointer --config-file=$HOME/.config/qtile/scripts/jgmenurc
