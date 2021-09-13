71560a6e60328d7339b7aceaeccc333f629db1bf
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-09-13 13:52:46 +0300

    Added ability to search dotfiles

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,3 +5,6 @@ map('n', '<leader>ff', ':Telescope find_files<cr>', default_opts)
 map('n', '<leader>fg', ':Telescope live_grep<cr>', default_opts)
 map('n', '<leader>fb', ':Telescope buffers<cr>', default_opts)
 map('n', '<leader>fh', ':Telescope help_tags<cr>', default_opts)
+
+-- Search dotfiles
+map('n', '<leader>fd', ':lua require("telescope.builtin").git_files({cwd = "~/dotfiles"})<CR>', default_opts)
