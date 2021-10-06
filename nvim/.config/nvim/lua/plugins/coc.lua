4a39752f83fd01c4f58b5e5f151c4c98885a76ea
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-10-06 10:04:44 +0300

    Added keybindings for CoC

--- nvim/.config/nvim/lua/plugins/coc.lua
+++ nvim/.config/nvim/lua/plugins/coc.lua
@@ -2,3 +2,5 @@ local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
 map('n', '<leader>cd', ':CocDiagnostics<CR>', default_opts)
+map('n', '<leader>cc', ':CocCommand<CR>', default_opts)
+map('n', '<leader>ca', ':CocAction<CR>', default_opts)
