505cc352ecd24c029a613cb918e597a472a8f521
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-10-13 17:30:23 +0300

    Open file and dir configs differently

--- qtile/.config/qtile/edit_config.sh
+++ qtile/.config/qtile/edit_config.sh
@@ -3,5 +3,10 @@
 config_name=`ls -la ~/.config | tail -n +4 | awk '{print $9}' | rofi -p "Edit Config" -dmenu`
 selected_config=$HOME/.config/$config_name
 
-alacritty -e nvim $selected_config
+if [[ -d "$selected_config" ]];
+then
+	cd $selected_config && alacritty -e nvim
+else
+	alacritty -e nvim $selected_config
+fi
 
