5a7ed0595a0993d02a9ead811dc6aa86cae708a9
Oleksandr Dembinskyi <alexander.dembinsky@gmail.com>
2021-10-13 13:01:37 +0300

    Change focus color of floating windows

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -131,7 +131,7 @@ dgroups_app_rules = []  # type: List
 follow_mouse_focus = False
 bring_front_click = "floating_only"
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
+floating_layout = layout.Floating(border_focus='#aaaaaa', float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
     Match(wm_class='confirmreset'),  # gitk
